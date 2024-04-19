90102d83fc72443bfd93bab934d56d04c0aff6ce
rajabboy <coder@rajabboys-MacBook-Pro.local>
2024-04-19 17:58:34 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layouts/navbar/Navbar";
 import Footer from "./components/layouts/footer/Footer";
 import Home from "./components/pages/home/Home";
+import "./App.css";
 function App() {
   return (
     <div className="wrapper">
--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
-import style from './Navbar.module.css'
+import React from "react";
+import style from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import logo from "../../../assets/icons/logo.svg";
+import basket from "../../../assets/icons/basket.svg";
+import burger from "../../../assets/icons/burger.svg";
+import user from "../../../assets/icons/user.svg";
+import search from "../../../assets/icons/search.svg";
+import heart from "../../../assets/icons/heart.svg";
+
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className={style.navbar}>
+      <div className="container">
+        <ul className={style.item_check}>
+          <li className={style.item_check_li}>
+            Формат 3 в 1 (аптека, ветеринарная аптека, медицинская техника и
+            товары)
+          </li>
+          <li className={style.item_check_li}>25 лет опыта</li>
+          <li className={style.item_check_li}>
+            Формацевтическое консультирование
+          </li>
+          <li className={style.item_check_li}>Быстрая и надежная доставка</li>
+        </ul>
+        <nav className={style.nav}>
+          <NavLink className={style.logo} to="/">
+            <img src={logo} alt="" />
+          </NavLink>
+          <div className={style.search_box}>
+            <input
+              type="text"
+              placeholder="Поиск по названию, производителю, действующему веществу или симптому"
+              className={style.search}
+            />
+            <button>
+              <img src={search} alt="" />
+            </button>
+          </div>
+          <NavLink to="" className={style.favorite_link}>
+            <img src={heart} alt="" />
+            Избранное
+          </NavLink>
+          <NavLink to="" className={style.favorite_link}>
+            <img src={basket} alt="" />
+            Корзина
+          </NavLink>
+          <NavLink to="" className={style.favorite_link}>
+            <img src={user} alt="" />
+            Войти
+          </NavLink>
+        </nav>
+
+        <nav className={style.nav_burger}>
+          <img src={burger} className={style.bur_photo} alt="" />
+          <ul className={style.item}>
+            <li className={style.list}>
+              <NavLink to="">Интернет-магазин </NavLink>
+            </li>
+            <li className={style.list}>
+              <NavLink to="">Главная </NavLink>
+            </li>
+            <li className={style.list}>
+              <NavLink to=""> О компании </NavLink>
+            </li>
+            <li className={style.list}>
+              <NavLink to=""> Помощь</NavLink>
+            </li>
+            <li className={style.list}>
+              <NavLink to=""> Акции </NavLink>
+            </li>
+            <li className={style.list}>
+              <NavLink to="">Новости</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
